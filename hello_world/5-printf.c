ef93dd176226e624256dd598019a86a1aa635f97
leila0705 <7575@holbertonstudents.com>
2023-10-13 14:50:25 -0700

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Prints out a string to stdout
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,);
+
+	return (0);
+}
